1e622e2c90aae232a800758d8296a30be40ec522
Bernt Popp <bernt.popp@gmail.com>
2023-12-29 20:27:41 +0100

    feat: add logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <v-app>
+    <!-- App bar -->
+    <v-app-bar app color="primary" dark>
+      <!-- Logo Image -->
+      <v-img
+        src="logo.png"
+        class="mr-3"
+        contain
+        max-height="48"
+        max-width="48"
+      ></v-img>
+
+      <!-- App Title -->
+      <v-toolbar-title>Gene Curator</v-toolbar-title>
+      <!-- Add more toolbar items here as needed -->
+    </v-app-bar>
+
+    <!-- Main content -->
     <v-main>
       <DataDisplayTable/>
     </v-main>
@@ -18,4 +35,5 @@ export default {
 </script>
 
 <style>
+/* Add any additional styles here */
 </style>
